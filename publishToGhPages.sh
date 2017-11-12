086b49f1b768bc9212d4dcc7c375f8848d7912d0
Nicholas Winder <nfxdevelopment@gmail.com>
2017-11-12 16:59:22 +0000

    auto mate website publishing.

---
+++ publishToGhPages.sh
@@ -0,0 +1,15 @@
+# remove previous publication
+rm -rf public
+mkdir public
+
+# clone gh-pages branch from the local repo into a repo located within "public"
+git clone .git --branch gh-pages public
+  
+# generate
+hugo
+  
+# commit the changes in the clone and push them back to the local gh-pages branch    
+cd public && git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages && cd ..
+
+# publish
+git push origin gh-pages
